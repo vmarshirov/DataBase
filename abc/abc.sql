b1531529e8f7f0f5b5cad467a1c247fbbe1e34ac
vmarshirov <vmarshirov@outlook.com>
2024-10-19 22:39:39 +0300

    Update abc.sql

--- abc/abc.sql
+++ abc/abc.sql
@@ -1,15 +1,18 @@
+DROP TABLE orm_abc_app_abcmodel;
+
 CREATE TABLE IF NOT EXISTS "orm_abc_app_abcmodel" 
 ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, 
-"task" varchar(255) NOT NULL, 
+"task" varchar(255) NOT NULL DEFAULT "Формулировка задачи", 
 "a" integer NOT NULL, 
 "b" integer NOT NULL, 
 "c" integer NOT NULL,
-"result" varchar(255) NOT NULL
+"result" varchar(255) NOT NULL DEFAULT "нет результата"
 )
 
 
 	
 INSERT INTO orm_abc_app_abcmodel ("task", "a", "b","c","result") VALUES("формулировка", 1, 2, 3, "Результат");
+INSERT INTO orm_abc_app_abcmodel ("a", "b","c") VALUES(1, 2, 3);
 SELECT * FROM  orm_abc_app_abcmodel;
 
 INSERT INTO orm_abc_app_abcmodel VALUES(100,"формулировка", 1, 2, 3, "Результат");
