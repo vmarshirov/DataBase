e9646494cafdaf93af0045708c50ce9e1468f535
vmarshirov <vmarshirov@outlook.com>
2024-10-19 22:28:48 +0300

    Update abc.sql

--- abc/abc.sql
+++ abc/abc.sql
@@ -12,21 +12,9 @@ CREATE TABLE IF NOT EXISTS "orm_abc_app_abcmodel"
 INSERT INTO orm_abc_app_abcmodel ("task", "a", "b","c","result") VALUES("формулировка", 1, 2, 3, "Результат");
 SELECT * FROM  orm_abc_app_abcmodel;
 
-INSERT INTO orm_abc_app_abcmodel VALUES(100,"формулировка", 1, 2, 3, "Результат")
-INSERT INTO orm_abc_app_abcmodel VALUES(?,"формулировка", 1, 2, 3, "Результат")
+INSERT INTO orm_abc_app_abcmodel VALUES(100,"формулировка", 1, 2, 3, "Результат");
+INSERT INTO orm_abc_app_abcmodel VALUES(?,"формулировка", 1, 20, 3, "Результат");
 	
 
-SELECT 	a, b, c FROM orm_abc_app_abcmodel;
-	CASE
-		WHEN c > a AND c < b THEN 'в диапазоне'
-		ELSE'вне диапазона'
-	END
-FROM orm_abc_app_abcmodel;
+SELECT a, b, c, CASE WHEN c > a AND c < b THEN 'с в диапазоне a-b' ELSE ' с вне диапазона a-b' END FROM orm_abc_app_abcmodel;
 
-
-SELECT 	a, b, c   
-CASE a,b
-	WHEN a > b THEN "a>b"
-	ELSE 'N'
-END	
-FROM orm_abc_app_abcmodel;
