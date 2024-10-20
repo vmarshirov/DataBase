61cfb7147a87d27203a312b63a7f94e02ad2155c
vmarshirov <vmarshirov@outlook.com>
2024-10-20 21:09:29 +0300

    ed

--- SQLite/SQLite.md
+++ SQLite/SQLite.md
@@ -1,6 +1,7 @@
-https://www.sqlitetutorial.net/
-
 
 https://www.sqlite.org/lang.html
 
 https://www.sqlite.org/syntax/column-constraint.html
+
+
+https://www.sqlitetutorial.net/
--- abc/abc.sql
+++ abc/abc.sql
@@ -1,8 +1,8 @@
-DROP TABLE orm_abc_app_abcmodel;
+DROP TABLE IF EXISTS orm_abc_app_abcmodel;
 
 CREATE TABLE IF NOT EXISTS "orm_abc_app_abcmodel" 
 ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, 
-"task" varchar(255) NOT NULL DEFAULT "Формулировка задачи", 
+"task" varchar(255) NOT NULL DEFAULT "Равна ли С сумме A и B ?", 
 "a" integer NOT NULL DEFAULT 10, 
 "b" integer NOT NULL DEFAULT 20, 
 "c" integer NOT NULL DEFAULT 30,
@@ -11,13 +11,16 @@ CREATE TABLE IF NOT EXISTS "orm_abc_app_abcmodel"
 
 
 	
-INSERT INTO orm_abc_app_abcmodel ("task", "a", "b","c","result") VALUES("формулировка", 1, 2, 3, "Результат");
-INSERT INTO orm_abc_app_abcmodel ("a", "b","c") VALUES(1, 2, 3);
+INSERT INTO orm_abc_app_abcmodel ("task", "a", "b","c","result") VALUES("Равна ли С сумме A и B ?", 1, 2, 3, "Результат");
 SELECT * FROM  orm_abc_app_abcmodel;
 
-INSERT INTO orm_abc_app_abcmodel VALUES(100,"формулировка", 1, 2, 3, "Результат");
-INSERT INTO orm_abc_app_abcmodel VALUES(?,"формулировка", 1, 20, 3, "Результат");
-	
+INSERT INTO orm_abc_app_abcmodel VALUES(100,"Равна ли С сумме A и B ?", 3, 2, 3, "Результат");
+INSERT INTO orm_abc_app_abcmodel VALUES(?,"Равна ли С сумме A и B ?", 1, 20, 3, "Результат");
 
-SELECT a, b, c, CASE WHEN c > a AND c < b THEN 'с в диапазоне a-b' ELSE ' с вне диапазона a-b' END FROM orm_abc_app_abcmodel;
+INSERT INTO orm_abc_app_abcmodel ("a", "b","c") VALUES(20, 10, 5);
 
+
+SELECT task AS Формулировка, a, b, c, CASE WHEN c > a AND c < b THEN 'с в диапазоне a-b' WHEN c > b AND c < a THEN 'с в диапазоне a-b' ELSE ' с вне диапазона a-b' END AS Результат FROM orm_abc_app_abcmodel;
+
+UPDATE orm_abc_app_abcmodel SET a = 100 WHERE b == 20
+SELECT * FROM  orm_abc_app_abcmodel;
